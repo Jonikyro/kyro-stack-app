e098bf978ac1042c1459249d1091e4f0f272e621
Joni Kyrönlahti <joni.kyronlahti@gmail.com>
2024-10-13 21:59:23 +0300

    React-aria localization provider

--- src/00.KyroStackApp.UI/src/main.tsx
+++ src/00.KyroStackApp.UI/src/main.tsx
@@ -3,12 +3,15 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { queryClient } from './data/query-client';
+import { LocalizationProvider } from './localization/localization-provider';
 import { RootRouter } from './routing/root-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<RootRouter />
+			<LocalizationProvider>
+				<RootRouter />
+			</LocalizationProvider>
 		</QueryClientProvider>
 	</React.StrictMode>
 );
