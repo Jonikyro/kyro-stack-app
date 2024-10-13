1f8033151c47a29a778f0e92d2924af5f8d5f8a4
Joni Kyrönlahti <joni.kyronlahti@gmail.com>
2024-10-13 22:25:33 +0300

    Small fixes

--- src/00.KyroStackApp.UI/src/routing/root-router.tsx
+++ src/00.KyroStackApp.UI/src/routing/root-router.tsx
@@ -4,7 +4,7 @@ import { anonymousRouter } from './anonymous-router';
 import { authenticatedRouter } from './authenticated-router';
 
 export function RootRouter() {
-	const [userIsAuthenticated] = useState(true);
+	const [userIsAuthenticated] = useState(true); // Replace with `const user = useSuspenseQuery(userQuery);`
 
 	if (!userIsAuthenticated) {
 		return <RouterProvider router={anonymousRouter} />;
