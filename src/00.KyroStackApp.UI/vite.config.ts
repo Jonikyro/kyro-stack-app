a9c79b7e24b4a3b3f6ee0c80ea91eeba239319b2
Joni Kyrönlahti <joni.kyronlahti@gmail.com>
2024-10-13 21:55:51 +0300

    Add preloading (release build only)

--- src/00.KyroStackApp.UI/vite.config.ts
+++ src/00.KyroStackApp.UI/vite.config.ts
@@ -1,10 +1,29 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		UnpluginInjectPreload({
+			files: [
+				{
+					outputMatch: /[a-z-0-9]*\.woff2$/,
+					attributes: {
+						type: 'font/woff2',
+						as: 'font',
+						crossorigin: 'anonymous'
+					}
+				},
+				{
+					outputMatch: /[a-z-0-9]*\.svg$/
+				}
+			],
+			injectTo: 'head-prepend'
+		})
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
