1ce11475a6c3048f9e1c2ef6d3fae86d4c5211a0
Joni Kyrönlahti <joni.kyronlahti@gmail.com>
2025-04-05 19:30:54 +0300

    Improve `Result` type

--- src/05.KyroStackApp.Shared/Functional/Result.cs
+++ src/05.KyroStackApp.Shared/Functional/Result.cs
@@ -1,18 +1,20 @@
-﻿namespace KyroStackApp.Shared.Functional;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KyroStackApp.Shared.Functional;
 
 /// <summary>
-/// Represents a type that is holding a value or an exception.
+/// Represents a type that is holding a value or an error.
 /// </summary>
-public readonly struct Result<TSuccessType>
+public readonly struct Result<TSuccessType, TErrorType>
 {
     internal readonly bool _isSuccess;
-    internal readonly TSuccessType _value;
-    internal readonly Exception? _error;
+    internal readonly TSuccessType? _value;
+    internal readonly TErrorType? _error;
 
-    private Result(Exception error)
+    private Result(TErrorType error)
     {
         this._isSuccess = false;
-        this._value = default!;
+        this._value = default;
         this._error = error;
     }
 
@@ -23,32 +25,72 @@ public readonly struct Result<TSuccessType>
         this._error = default;
     }
 
-    public static Result<TSuccessType> Success(TSuccessType success)
+    [MemberNotNullWhen(true, nameof(_value))]
+    [MemberNotNullWhen(false, nameof(_error))]
+    public bool IsSuccess => _isSuccess;
+
+    [MemberNotNullWhen(false, nameof(_value))]
+    [MemberNotNullWhen(true, nameof(_error))]
+    public bool IsFailure => !_isSuccess;
+
+    public static Result<TSuccessType, TErrorType> Success(TSuccessType success)
     {
-        return new Result<TSuccessType>(success);
+        return new Result<TSuccessType, TErrorType>(success);
     }
 
-    public static Result<TSuccessType> Fail(Exception error)
+    public static Result<TSuccessType, TErrorType> Fail(TErrorType error)
     {
-        return new Result<TSuccessType>(error);
+        return new Result<TSuccessType, TErrorType>(error);
     }
 
     public TReturnType Match<TReturnType>(
         Func<TSuccessType, TReturnType> success,
-        Func<Exception, TReturnType> fail)
+        Func<TErrorType, TReturnType> fail)
     {
-        return this._isSuccess
+        return this.IsSuccess
             ? success(this._value)
-            : fail(this._error!);
+            : fail(this._error);
     }
 
-    public static implicit operator Result<TSuccessType>(TSuccessType value)
+    public Task<TReturnType> Match<TReturnType>(
+        Func<TSuccessType, Task<TReturnType>> success,
+        Func<TErrorType, Task<TReturnType>> fail)
     {
-        return new Result<TSuccessType>(value);
+        return this.IsSuccess
+            ? success(this._value)
+            : fail(this._error);
     }
 
-    public static implicit operator Result<TSuccessType>(Exception error)
+    public void Match(
+        Action<TSuccessType> success,
+        Action<TErrorType> fail)
     {
-        return new Result<TSuccessType>(error);
+        if (this.IsSuccess)
+        {
+            success(this._value);
+        }
+        else
+        {
+            fail(this._error);
+        }
+    }
+
+    public Task Match(
+        Func<TSuccessType, Task> success,
+        Func<TErrorType, Task> fail)
+    {
+        return this.IsSuccess
+            ? success(this._value)
+            : fail(this._error);
+    }
+
+    public static implicit operator Result<TSuccessType, TErrorType>(TSuccessType value)
+    {
+        return new Result<TSuccessType, TErrorType>(value);
+    }
+
+    public static implicit operator Result<TSuccessType, TErrorType>(TErrorType error)
+    {
+        return new Result<TSuccessType, TErrorType>(error);
     }
 }
