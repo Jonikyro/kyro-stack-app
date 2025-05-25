2fc730a1fcc085e73e32ed487c1ff4e5e40bbfff
Joni Kyrönlahti <joni.kyronlahti@gmail.com>
2025-05-25 15:08:30 +0300

    Remove init.js (not needed with scrollbar-gutter: stable;)

--- src/00.KyroStackApp.UI/public/init.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-	const scrollbarWidth =
-		window.innerWidth - document.documentElement.clientWidth;
-
-	document.documentElement.style.setProperty(
-		'--root-scrollbar-width',
-		scrollbarWidth + 'px'
-	);
-};
