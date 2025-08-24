b2d3fe83469d9b08f72642d967d539e293d4aed4
JoniKyro <joni.kyronlahti@gmail.com>
2025-08-24 13:54:20 +0300

    format

--- src/00.KyroStackApp.UI/src/main.tsx
+++ src/00.KyroStackApp.UI/src/main.tsx
@@ -5,12 +5,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { queryClient } from './data/query-client';
 import { LocalizationProvider } from './localization/localization-provider';
-
 import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
 	interface Register {
 		router: typeof router;
