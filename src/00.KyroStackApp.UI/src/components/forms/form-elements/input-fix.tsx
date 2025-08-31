6ac196b00b3adb96735a1bcdba4d683f379f7eeb
JoniKyro <joni.kyronlahti@gmail.com>
2025-09-01 00:43:19 +0300

    Move styles to css file

--- src/00.KyroStackApp.UI/src/components/forms/form-elements/input-fix.tsx
+++ src/00.KyroStackApp.UI/src/components/forms/form-elements/input-fix.tsx
@@ -1,19 +1,18 @@
-import { cn } from '@/utils/cn';
 import { ComponentPropsWithoutRef } from 'react';
 
 export type InputFixProps = {
 	inputId: string;
 } & Omit<ComponentPropsWithoutRef<'label'>, 'aria-hidden' | 'htmlFor'>;
 
+import './input-fix.css';
+
 export function InputFix({ inputId, className, ...rest }: InputFixProps) {
 	return (
 		<label
 			htmlFor={inputId}
 			aria-hidden
-			className={cn(
-				'inline-flex items-center bg-surface-container p-[0.5ch]',
-				className
-			)}
+			data-component='input-fix'
+			className={className}
 			{...rest}
 		/>
 	);
