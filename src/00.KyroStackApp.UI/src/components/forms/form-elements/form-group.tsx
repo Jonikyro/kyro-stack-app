2aa67a9a01649c37048f9459a5fb020a270480fa
JoniKyro <joni.kyronlahti@gmail.com>
2025-09-01 00:43:10 +0300

    Move styles to css file

--- src/00.KyroStackApp.UI/src/components/forms/form-elements/form-group.tsx
+++ src/00.KyroStackApp.UI/src/components/forms/form-elements/form-group.tsx
@@ -1,5 +1,6 @@
-import { cn } from '@/utils/cn';
 import { ComponentPropsWithoutRef } from 'react';
+
+import './form-group.css';
 
 const DEFAULT_ELEMENT = 'div';
 
@@ -10,11 +11,5 @@ type FormGroupProps<T extends HtmlElementTagName = 'div'> = {
 export function FormGroup({ as, className, ...rest }: FormGroupProps) {
 	const As = as ?? DEFAULT_ELEMENT;
 
-	return (
-		<As
-			className={cn('grid grid-cols-1 gap-4 py-4 md:grid-cols-2', className)}
-			data-component='form-group'
-			{...rest}
-		/>
-	);
+	return <As className={className} data-component='form-group' {...rest} />;
 }
