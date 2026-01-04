6f3d0dd266aa25799a054a9573b4ea9003a5c08c
JoniKyro <joni.kyronlahti@gmail.com>
2026-01-04 18:34:11 +0200

    Add documentation

--- src/05.KyroStackApp.Shared/ErrorCodeException.cs
+++ src/05.KyroStackApp.Shared/ErrorCodeException.cs
@@ -1,26 +1,33 @@
 ﻿namespace KyroStackApp.Shared;
 
-[Serializable]
+/// <summary>
+/// Type of <see cref="Exception"/> that is used to represent errors with specific error codes.
+/// These error codes are usually documented in the API documentation.
+/// <see cref="ErrorCodeException"/> should have a global error handler that converts the exception
+/// to a format that is suitable for API responses.
+/// </summary>
+/// 
+/// <remarks>
+/// It's often a good idea to inherit from this class to create named <see cref="ErrorCodeException"/>s.
+/// </remarks>
 public class ErrorCodeException : Exception
 {
-    public ErrorCodeException(string errorCode)
-    {
-        this.ErrorCode = errorCode;
-    }
-
     public ErrorCodeException(
         string errorCode, 
         int? suggestedStatusCode = null,
         string? message = null,
         Exception? innerException = null,
         object? parameters = null
-        ) : base(message, innerException)
+        ) : base(message ?? errorCode, innerException)
     {
         this.ErrorCode = errorCode;
         this.SuggestedStatusCode = suggestedStatusCode;
         this.Parameters = parameters;
     }
 
+    /// <summary>
+    /// Unique code that identifies the error
+    /// </summary>
     public string ErrorCode { get; }
 
     /// <summary>
